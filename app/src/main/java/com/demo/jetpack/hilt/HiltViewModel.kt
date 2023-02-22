7325712b40206d1bf2e3c34a1b577c624286c706
songhuan <songhuan@fanletech.com>
2023-02-22 14:35:02 +0800

    hilt 使用

--- app/src/main/java/com/demo/jetpack/hilt/HiltViewModel.kt
+++ app/src/main/java/com/demo/jetpack/hilt/HiltViewModel.kt
@@ -4,7 +4,6 @@ import androidx.lifecycle.ViewModel
 import dagger.hilt.android.scopes.ViewModelScoped
 import javax.inject.Inject
 
-@ViewModelScoped
 class HiltViewModel @Inject constructor(val mRepository: Repository) : ViewModel() {
 
 
